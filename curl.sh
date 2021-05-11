7f1f45180bedd0dfb80e3ab1cf97dcc07c1f0f1b
James Newell <james@jameslnewell.dev>
2021-05-11 17:15:53 +1000

    only change origin when newly created

--- curl.sh
+++ curl.sh
@@ -12,9 +12,8 @@ else
   mkdir -p $repo_directory
   git clone https://github.com/jameslnewell/dotfiles.git $repo_directory 
   cd $repo_directory 
+  # Swap HTTP to SSH for easier authentication
+  git remote set-url origin git@github.com:jameslnewell/dotfiles.git
 fi
 
 $repo_directory/main.sh
-
-# Swap HTTP to SSH for easier authentication
-git remote set-url origin git@github.com:jameslnewell/dotfiles.git
