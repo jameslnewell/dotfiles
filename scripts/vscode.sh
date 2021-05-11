23a4b02b309dc4a7580e577221d3a58589062b7d
James Newell <james@jameslnewell.dev>
2021-05-12 00:02:09 +1000

    vscode config

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -11,9 +11,13 @@ if [ ! -d "/Applications/Visual Studio Code.app" ]; then
   brew install --cask visual-studio-code
 fi
 
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+
 # install CLI
 cp ./configs/.config/fish/conf.d/vscode.fish ~/.config/fish/conf.d/vscode.fish
 
+# install extensions
 code --install-extension editorconfig.editorconfig --force
 code --install-extension dracula-theme.theme-dracula --force
 code --install-extension dbaeumer.vscode-eslint --force
@@ -21,6 +25,10 @@ code --install-extension jpoissonnier.vscode-styled-components --force
 code --install-extension ms-azuretools.vscode-docker --force
 code --install-extension mikestead.dotenv --force
 code --install-extension vscodevim.vim --force
+
+# configure vscode
+ln -sf $(pwd)/configs/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf $(pwd)/configs/Library/Application\ Support/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 print_section_setup_complete
 
